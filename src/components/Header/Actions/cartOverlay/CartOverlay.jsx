f0f682300bfa9ddc086cb44bf63198273172edf4
MohamedOMourad <mohamed.o.murad@gmail.com>
2022-11-25 03:07:21 +0200

    cart over lay

--- src/components/Header/Actions/cartOverlay/CartOverlay.jsx
+++ src/components/Header/Actions/cartOverlay/CartOverlay.jsx
@@ -8,7 +8,7 @@ class CartOverlay extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            cartIsOpen: false
+            cartIsOpen: true
         }
     }
 
--- src/components/Header/Actions/cartOverlay/cartItems/CartItems.jsx
+++ src/components/Header/Actions/cartOverlay/cartItems/CartItems.jsx
@@ -8,7 +8,141 @@ class CartItems extends Component {
         return (
             <div className={styles.container}>
                 <div className={styles['items-container']}>
-
+                    <p style={{ display: 'flex' }}>
+                        <span>My Bag </span>
+                        <span>0 </span>
+                        <span>items</span>
+                    </p>
+                    <div className={styles.scrolls}>
+                        <div className={styles['item-container']}>
+                            <div className={styles.description}>
+                                <div>brand</div>
+                                <div>name</div>
+                                <div>price</div>
+                                <div>
+                                    <div>size:</div>
+                                    <div className={styles.sizes}>
+                                        <div>xs</div>
+                                        <div>s</div>
+                                        <div>m</div>
+                                        <div>l</div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div>color:</div>
+                                    <div className={styles.sizes}>
+                                        <div>xs</div>
+                                        <div>s</div>
+                                        <div>m</div>
+                                        <div>l</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.update}>
+                                <button>+</button>
+                                <span>0</span>
+                                <button>-</button>
+                            </div>
+                            <div style={{ backgroundImage: `url(${'https://cdn.shopify.com/s/files/1/0087/6193/3920/products/DD1381200_DEOA_2_720x.jpg?v=1612816087'})` }} className={styles.image}> </div>
+                        </div>
+                        <div className={styles['item-container']}>
+                            <div className={styles.description}>
+                                <div>brand</div>
+                                <div>name</div>
+                                <div>price</div>
+                                <div>
+                                    <div>size:</div>
+                                    <div className={styles.sizes}>
+                                        <div>xs</div>
+                                        <div>s</div>
+                                        <div>m</div>
+                                        <div>l</div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div>color:</div>
+                                    <div className={styles.sizes}>
+                                        <div>xs</div>
+                                        <div>s</div>
+                                        <div>m</div>
+                                        <div>l</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.update}>
+                                <button>+</button>
+                                <span>0</span>
+                                <button>-</button>
+                            </div>
+                            <div style={{ backgroundImage: `url(${'https://cdn.shopify.com/s/files/1/0087/6193/3920/products/DD1381200_DEOA_2_720x.jpg?v=1612816087'})` }} className={styles.image}> </div>
+                        </div>
+                        <div className={styles['item-container']}>
+                            <div className={styles.description}>
+                                <div>brand</div>
+                                <div>name</div>
+                                <div>price</div>
+                                <div>
+                                    <div>size:</div>
+                                    <div className={styles.sizes}>
+                                        <div>xs</div>
+                                        <div>s</div>
+                                        <div>m</div>
+                                        <div>l</div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div>color:</div>
+                                    <div className={styles.sizes}>
+                                        <div>xs</div>
+                                        <div>s</div>
+                                        <div>m</div>
+                                        <div>l</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.update}>
+                                <button>+</button>
+                                <span>0</span>
+                                <button>-</button>
+                            </div>
+                            <div style={{ backgroundImage: `url(${'https://cdn.shopify.com/s/files/1/0087/6193/3920/products/DD1381200_DEOA_2_720x.jpg?v=1612816087'})` }} className={styles.image}> </div>
+                        </div>
+                        <div className={styles['item-container']}>
+                            <div className={styles.description}>
+                                <div>brand</div>
+                                <div>name</div>
+                                <div>price</div>
+                                <div>
+                                    <div>size:</div>
+                                    <div className={styles.sizes}>
+                                        <div>xs</div>
+                                        <div>s</div>
+                                        <div>m</div>
+                                        <div>l</div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div>color:</div>
+                                    <div className={styles.sizes}>
+                                        <div>xs</div>
+                                        <div>s</div>
+                                        <div>m</div>
+                                        <div>l</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.update}>
+                                <button>+</button>
+                                <span>0</span>
+                                <button>-</button>
+                            </div>
+                            <div style={{ backgroundImage: `url(${'https://cdn.shopify.com/s/files/1/0087/6193/3920/products/DD1381200_DEOA_2_720x.jpg?v=1612816087'})` }} className={styles.image}> </div>
+                        </div>
+                    </div>
+                    <div className={styles.checkout}>
+                        <button>view bag</button>
+                        <button>checkout</button>
+                    </div>
                 </div>
             </div>
         )
